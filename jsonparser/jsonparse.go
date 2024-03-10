2ebeb9a0e555be9b9d76d0ffce808aa98c61b6b8
Rubicon-Dev <dev@rubiconsa.com>
2024-03-10 15:25:47 +0200

    json parser update

--- jsonparser/jsonparse.go
+++ jsonparser/jsonparse.go
@@ -1,14 +1,24 @@
 package jsonparser
 
-import "errors"
+import (
+	"encoding/json"
+	"errors"
+)
 
 type Json struct {
+	Data interface{}
 }
 
 func handleParseObject(jsonData []byte) (bool, error) {
 	if len(jsonData) == 0 {
-		myError := errors.New("valid json, but empty")
+		myError := errors.New("valid JSON, but empty")
 		return false, myError
+	}
+
+	var parsedData interface{}
+	err := json.Unmarshal(jsonData, &parsedData)
+	if err != nil {
+		return false, err
 	}
 
 	return true, nil
--- jsonparser/jsonparser_test.go
+++ jsonparser/jsonparser_test.go
@@ -2,14 +2,29 @@ package jsonparser
 
 import "testing"
 
-func TestJsonEmptyorNil_handleParseObject(t *testing.T) {
-	var emptyString []byte
-	expectedErrorMessage := "valid json, but empty"
+func TestJsonValidJson_handleParseObject(t *testing.T) {
+	jsonData := []byte(`{"key": "value"}`)
+	//expectedResult := true
 
-	result, err := handleParseObject(emptyString)
+	result, err := handleParseObject(jsonData)
+
+	if !result {
+		t.Errorf("Expected true: %t", result)
+	}
+
+	if err != nil {
+		t.Errorf("Expected no error, but got: %s", err.Error())
+	}
+}
+
+func TestJsonInvalidJson_handleParseObject(t *testing.T) {
+	invalidJson := []byte(`{"key": value}`)
+	expectedErrorMessage := "invalid character 'v' looking for beginning of value"
+
+	result, err := handleParseObject(invalidJson)
 
 	if result {
-		t.Errorf("Expected true: %s", result)
+		t.Errorf("Expected false: %t", result)
 	}
 
 	if err == nil {
